a072972fc8f3aa84d3c1480c8a8a0f2ba9236c27
NGUYỄN HỒ SỸ PHÚ <nguyenhosyphu@gmail.com>
2025-11-08 23:27:26 +0700

    Cập nhật wedconfig

--- src/main/java/com/example/fashion/config/WebConfig.java
+++ src/main/java/com/example/fashion/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.fashion.config; // Đảm bảo đúng package 'config'
+package com.example.fashion.config; // Đảm bảo đúng package của bạn
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Áp dụng cho tất cả API bắt đầu bằng /api/
-                .allowedOrigins("http://localhost:3001") // Cho phép máy chủ React
+                .allowedOrigins("http://localhost:3000") // Cho phép máy chủ React
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Các phương thức
                 .allowedHeaders("*") // Cho phép tất cả các header
                 .allowCredentials(true);
